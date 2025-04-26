8e64729fac710cc7bd679b6a671cf2ecea54ce13
Joël VIGREUX <joel@udixio.fr>
2025-04-26 11:33:29 +0200

    feat: add animated background with framer-motion
    Integrate `framer-motion` to animate background visibility and transitions. Refactor `Background` component to support conditional rendering with animations. Update `index.astro` to include the new animated background for improved user experience.

--- src/components/three/Background.tsx
+++ src/components/three/Background.tsx
@@ -4,6 +4,7 @@ import {NoToneMapping} from "three";
 import {ControlledCamera} from "@components/three/ControlledCamera.tsx";
 import {BackgroundColor} from "./BackgroundColor";
 import {BackgroundStar} from "@components/three/BackgroundStar.tsx";
+import {AnimatePresence, motion, useInView} from "framer-motion";
 
 export const Background = ({
                                count = 20,
@@ -13,26 +14,39 @@ export const Background = ({
                            }) => {
     const canvasRef = useRef<HTMLDivElement>(null); // Référence pour le conteneur du `Canvas`
 
+    const isInView = useInView(canvasRef)
 
     return (
         <div ref={canvasRef}
-             className={` fixed -z-10   top-0 h-screen w-screen !fixed"  ${className}`}>
+             className={` absolute -z-10   top-0 h-screen w-screen ${className}`}>
+            <AnimatePresence>
+                {
+                    isInView && (
+                        <motion.div
+                            initial={{opacity: 0,}}
+                            animate={{opacity: 1,}}
+                            exit={{opacity: 0,}}
+                            transition={{duration: 1, ease: "easeInOut",}}
+                            className={"w-full h-full"}>
+                            <Canvas
+                                gl={{toneMapping: NoToneMapping}}
+                                className={"blur-xl  opacity-30"}
+                            >
+                                <ControlledCamera canvasRef={canvasRef}/>
+                                <BackgroundColor count={10} size={20}/>
 
-            <Canvas
-                gl={{toneMapping: NoToneMapping}}
-                className={"blur-xl  opacity-30"}
-            >
-                <ControlledCamera canvasRef={canvasRef}/>
-                <BackgroundColor count={10} size={20}/>
+                            </Canvas>
+                            <Canvas
+                                className={"top-0 !absolute opacity-90 "}
+                            >
+                                <ControlledCamera canvasRef={canvasRef}/>
 
-            </Canvas>
-            <Canvas
-                className={"top-0 !absolute opacity-90 "}
-            >
-                <ControlledCamera canvasRef={canvasRef}/>
-
-                <BackgroundStar count={1000}/>
-            </Canvas>
+                                <BackgroundStar count={1000}/>
+                            </Canvas>
+                        </motion.div>
+                    )
+                }
+            </AnimatePresence>
         </div>
     );
 };
